8e2ebb1047301f9dcc1470735fb6f51aa8f17ef3
Ammar Yasser <ammar.yasser@chefaa.com>
2025-09-02 13:06:11 +0300

    update ImagePicker auto with selected tab mode

--- src/components/ImagePicker.tsx
+++ src/components/ImagePicker.tsx
@@ -1,84 +1,47 @@
-import React, { useState } from 'react'
-import { View, Text, Image, Pressable, Modal, StyleSheet } from 'react-native'
+import React from 'react'
+import { View, Text, Image, Pressable, StyleSheet } from 'react-native'
 import { Colors } from '../theme/Colors'
 import { AppIcons } from '../constants/icons'
+import { ActiveTab } from '../types/Tab'
 
 interface ImagePickerProps {
+  tab: ActiveTab
   photoUri?: string | null
   onPickCamera: () => void
   onPickGallery: () => void
 }
 
 const ImagePicker: React.FC<ImagePickerProps> = ({
+  tab,
   photoUri,
   onPickCamera,
   onPickGallery,
 }) => {
-  const [modalVisible, setModalVisible] = useState(false)
-
-  const handleOpenOptions = () => setModalVisible(true)
-  const handleCloseOptions = () => setModalVisible(false)
+  const handlePickAction = tab == ActiveTab.Found ? onPickCamera : onPickGallery
 
   return (
-    <>
-      <Pressable
-        style={styles.root}
-        onPress={handleOpenOptions}
-      >
-        {photoUri ? (
-          <Image
-            style={styles.image}
-            source={{ uri: photoUri }}
-            resizeMode="cover"
-          />
-        ) : (
-          <View style={[styles._dashed_border, styles.placeholderView]}>
-            {AppIcons.camera(40, Colors.gray)}
-
-            <Text style={styles.placeholderText}>Click to add photo</Text>
-          </View>
-        )}
-      </Pressable>
-
-      {/* Bottom Action Sheet */}
-      <Modal
-        animationType="fade"
-        transparent
-        visible={modalVisible}
-        onRequestClose={handleCloseOptions}
-      >
-        <Pressable
-          style={styles.bottomSheetContainer}
-          onPress={handleCloseOptions}
-        >
-          <View style={styles.sheetActionsContainer}>
-            <Pressable
-              style={[styles._dashed_border, styles.sheetAction]}
-              onPress={() => {
-                handleCloseOptions()
-                onPickCamera()
-              }}
-            >
-              {AppIcons.camera(32, Colors.lightGray)}
-
-              <Text style={styles.sheetActionText}>From Camera</Text>
-            </Pressable>
-
-            <Pressable
-              style={[styles._dashed_border, styles.sheetAction]}
-              onPress={() => {
-                handleCloseOptions()
-                onPickGallery()
-              }}
-            >
-              {AppIcons.gallery(32, Colors.lightGray)}
-
-              <Text style={styles.sheetActionText}>From Gallery</Text>
-            </Pressable>
-          </View>
-        </Pressable>
-      </Modal>
-    </>
+    <Pressable
+      style={styles.root}
+      onPress={handlePickAction}
+    >
+      {photoUri ? (
+        <Image
+          style={styles.image}
+          source={{ uri: photoUri }}
+          resizeMode="cover"
+        />
+      ) : tab == ActiveTab.Found ? (
+        <View style={[styles._dashed_border, styles.placeholderView]}>
+          {AppIcons.camera(40, Colors.gray)}
+          <Text style={styles.placeholderText}>Capture photo</Text>
+        </View>
+      ) : (
+        <View style={[styles._dashed_border, styles.placeholderView]}>
+          {AppIcons.gallery(40, Colors.gray)}
+          <Text style={styles.placeholderText}>Attach image</Text>
+        </View>
+      )}
+    </Pressable>
   )
 }
 
@@ -107,30 +70,6 @@ const styles = StyleSheet.create({
   },
   placeholderText: {
     marginTop: 6,
-    color: Colors.gray,
-  },
-
-  bottomSheetContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-    backgroundColor: 'rgba(0,0,0,0.2)',
-  },
-  sheetActionsContainer: {
-    backgroundColor: Colors.white,
-    padding: 16,
-    borderRadius: 8,
-    flexDirection: 'row',
-    gap: 16,
-    justifyContent: 'center',
-  },
-  sheetAction: {
-    padding: 16,
-    alignItems: 'center',
-    gap: 12,
-  },
-  sheetActionText: {
     color: Colors.gray,
   },
 
