0c18086642485ddac29a62190996a854009fa1df
Ammar Yasser <ammar.yasser@chefaa.com>
2025-09-02 13:15:16 +0300

    navigation: add SCREEN_KEYS and RootStackParamList

---
+++ src/navigation/ScreenKeys.ts
@@ -0,0 +1,15 @@
+import { ItemType } from '../types/Item'
+
+// Screen keys
+export const SCREEN_KEYS = {
+  Home: 'HomeScreen',
+  ItemDetails: 'ItemDetailsScreen',
+  ReportItem: 'ReportItemScreen',
+}
+
+// Root Stack Params
+export type RootStackParamList = {
+  HomeScreen: undefined
+  ItemDetailsScreen: { itemId: number }
+  ReportItemScreen: { type: ItemType }
+}
