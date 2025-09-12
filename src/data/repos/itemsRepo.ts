59996a2a857ec54d01675f7a00f9de41e979efd4
Ammar Yasser <ammar.yasser@chefaa.com>
2025-09-12 04:07:45 +0300

    use firestore data source in item repo

--- src/data/repos/itemsRepo.ts
+++ src/data/repos/itemsRepo.ts
@@ -1,7 +1,7 @@
 import { Item, ItemType } from '../../domain/entities/Item'
 import { ItemRepoInterface } from '../../domain/repos/ItemRepoInterface'
 import { itemsSQLiteDS } from '../sources/local/itemsSQLiteDS'
-// import { itemsFirebaseDS } from '../sources/remote/itemsFirebaseDS'
+import { itemsFirebaseDS } from '../sources/remote/itemsFirebaseDS'
 
 export const itemsRepo: ItemRepoInterface = {
   async init() {
@@ -23,43 +23,43 @@ export const itemsRepo: ItemRepoInterface = {
   async add(item: Omit<Item, 'id'>) {
     // insert locally first
     const saved = await itemsSQLiteDS.add(item)
-    // // try remote sync but don't block on failure
-    // try {
-    //   await itemsFirebaseDS.upsert(saved)
-    // } catch (err) {
-    //   console.warn('Remote upsert failed (add):', err)
-    // }
+    // try remote sync but don't block on failure
+    try {
+      await itemsFirebaseDS.upsert(saved)
+    } catch (err) {
+      console.error('Remote upsert failed (add):', err)
+    }
     return saved
   },
 
   async update(item) {
     await itemsSQLiteDS.update(item)
 
-    // try {
-    //   await itemsFirebaseDS.upsert(item)
-    // } catch (err) {
-    //   console.warn('Remote upsert failed (update):', err)
-    // }
+    try {
+      await itemsFirebaseDS.upsert(item)
+    } catch (err) {
+      console.error('Remote upsert failed (update):', err)
+    }
   },
 
   async upsert(item) {
     await itemsSQLiteDS.upsert(item)
 
-    // try {
-    //   await itemsFirebaseDS.upsert(item)
-    // } catch (err) {
-    //   console.warn('Remote upsert failed (upsert):', err)
-    // }
+    try {
+      await itemsFirebaseDS.upsert(item)
+    } catch (err) {
+      console.error('Remote upsert failed (upsert):', err)
+    }
   },
 
   async remove(id) {
     await itemsSQLiteDS.remove(id)
 
-    // try {
-    //   await itemsFirebaseDS.remove(id)
-    // } catch (err) {
-    //   console.warn('Remote remove failed:', err)
-    // }
+    try {
+      await itemsFirebaseDS.remove(id)
+    } catch (err) {
+      console.error('Remote remove failed:', err)
+    }
   },
 
   async toggleResolved(id) {
@@ -67,12 +67,12 @@ export const itemsRepo: ItemRepoInterface = {
 
     const updated = await itemsSQLiteDS.getById(id)
 
-    // if (updated) {
-    //   try {
-    //     await itemsFirebaseDS.upsert(updated)
-    //   } catch (err) {
-    //     console.warn('Remote markResolved failed:', err)
-    //   }
-    // }
+    if (updated) {
+      try {
+        await itemsFirebaseDS.upsert(updated)
+      } catch (err) {
+        console.error('Remote markResolved failed:', err)
+      }
+    }
   },
 }
