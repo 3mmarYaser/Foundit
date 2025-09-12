2dcd102848a6ba4aff15e90f6aa553fdfef98c6f
Ammar Yasser <ammar.yasser@chefaa.com>
2025-09-12 04:06:37 +0300

    firebase: add firestore data source

---
+++ src/data/sources/remote/itemsFirebaseDS.ts
@@ -0,0 +1,26 @@
+import { Item } from '../../../domain/entities/Item'
+import { itemMapper } from '../../mappers/itemMapper'
+import { firestoreClient } from '../../../services/firebase/firestore'
+
+//
+
+export const itemsFirebaseDS = {
+  async getAll(): Promise<Item[]> {
+    const remotes = await firestoreClient.getAllRemote()
+    return remotes.map(r => itemMapper.toDomain(r))
+  },
+
+  async getById(id: number): Promise<Item | null> {
+    const r = await firestoreClient.getById(id)
+    return r ? itemMapper.toDomain(r) : null
+  },
+
+  async upsert(item: Item): Promise<void> {
+    const remote = itemMapper.toRemoteRow(item)
+    await firestoreClient.upsertRemote(remote)
+  },
+
+  async remove(id: number): Promise<void> {
+    await firestoreClient.deleteRemote(id)
+  },
+}
